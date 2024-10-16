5c98055f7631f9565252278ccb4dc9521b4ec990
Marq Gonzales <Marq.Gonzales@tdk.com>
2024-10-16 10:41:36 +0800

    ongoing development phase

--- ISDSupportLogsManager.Shared/Model/Support.cs
+++ ISDSupportLogsManager.Shared/Model/Support.cs
@@ -16,18 +16,11 @@ namespace ISDSupportLogsManager.Shared.Model
         public Category Category { get; set; } = new Category();
         public User User { get; set; } = new User();
 
-        public string SectionValue => Section.Name;
-        public string ShiftValue => Shift.Name;
-        public string BizValue => Biz.Name;
-        public string DepartmentValue => Department.Name;
-        public string ScopeValue => Scope.Name;
-        public string CategoryValue => Category.Name;
-        public string UserValue => $"{User.FirstName} {User.LastName}";
-
         public DateOnly StartDate { get; set; }
         public TimeOnly StartTime { get; set; }
         public DateOnly EndDate { get; set; }
         public TimeOnly EndTime { get; set; }
+
         public int ElapsedMinutes
         {
             get
@@ -47,5 +40,45 @@ namespace ISDSupportLogsManager.Shared.Model
         public string Details { get; set; } = string.Empty;
         public string Cause { get; set; } = string.Empty;
         public string ActionTaken { get; set; } = string.Empty;
+    }
+
+    public class SupportLookup
+    {
+        public string ControlNo { get; set; } = string.Empty;
+        public string SectionName { get; set; } = string.Empty;
+        public string ShiftName { get; set; } = string.Empty;
+        public string BizName { get; set; } = string.Empty;
+        public string DepartmentName { get; set; } = string.Empty;
+        public string ScopeName { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string UserFirstName { get; set; } = string.Empty;
+        public string UserLastName { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
+        public string Cause { get; set; } = string.Empty;
+        public string ActionTaken { get; set; } = string.Empty;
+
+        public DateOnly StartDate { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public DateOnly EndDate { get; set; }
+        public TimeOnly EndTime { get; set; }
+
+        public int ElapsedMinutes
+        {
+            get
+            {
+                DateTime startDateTime = StartDate.ToDateTime(StartTime);
+                DateTime endDateTime = EndDate.ToDateTime(EndTime);
+
+                TimeSpan elapsed = endDateTime - startDateTime;
+
+                return (int)elapsed.TotalMinutes;
+            }
+        }
+
+        public DateTime StartDateTime => StartDate.ToDateTime(StartTime);
+        public DateTime EndDateTime => StartDate.ToDateTime(EndTime);
+
+        public string UserFullName => $"{UserFirstName} {UserLastName}";
     }
 }
