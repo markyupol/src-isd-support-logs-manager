ce2ff33730fef2cd792bdce40d9c30e553c4848f
Marq Gonzales <Marq.Gonzales@tdk.com>
2024-10-19 19:06:35 +0800

    ongoing development phase

---
+++ ISDSupportLogsManager.Shared/Model/Reports/CategoryPerDayReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISDSupportLogsManager.Shared.Model.Reports
+{
+    public class CategoryPerDayReport
+    {
+        public DateTime Day { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public int Count { get; set; }
+    }
+}
